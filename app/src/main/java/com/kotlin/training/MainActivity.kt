0b4a4eef0a0d3429124a47cec490eae15986c7d1
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-09-13 08:25:59 +0100

    Improve counter demo

--- app/src/main/java/com/kotlin/training/MainActivity.kt
+++ app/src/main/java/com/kotlin/training/MainActivity.kt
@@ -2,10 +2,29 @@ package com.kotlin.training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val plusButton = findViewById<Button>(R.id.plusButton)
+        val minusButton = findViewById<Button>(R.id.minusButton)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        var counter = 0
+
+        plusButton.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+        }
+
+        minusButton.setOnClickListener {
+            counter--
+            textView.text = counter.toString()
+        }
+
     }
 }
